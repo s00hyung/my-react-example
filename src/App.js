bb1e852a9cb0369733f03583a97fb41e733fb4f3
SooHyung Lee <18585776+soohl@users.noreply.github.com>
2021-05-08 11:39:43 +0900

    dd

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
-import ChildComponent from './example/ChildComponent';
+import ChildProperty from './jsx.example/ChildProperty';
+import DefaultPropsComponent from './jsx.example/DefaultPropsComponent';
 
 class App extends Component {
   render() {
     return (
-      <ChildComponent
-        boolValue={true}
-        numValue={1}
-        arrayValue={[1, 2, 3]}
-        objValue={{ name: '제목', age: 30 }}
-        nodeValue={<h1>노드</h1>}
-        fnucValue={() => {
-          console.log('메시지');
-        }}
-      />
+      <div>
+        <ChildProperty>
+          <div>
+            <span>자식노드</span>
+          </div>
+        </ChildProperty>
+      </div>
     );
   }
 }
